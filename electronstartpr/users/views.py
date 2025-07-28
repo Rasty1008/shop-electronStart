7195b8a6bf52c10ec9d11820e48e043db60b4acb
Rostislav <teterukrostislav@gmail.com>
2025-07-29 00:37:19 +0500

    add UserLoginView

--- electronstartpr/users/views.py
+++ electronstartpr/users/views.py
@@ -1,36 +1,54 @@
 from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth.views import LoginView
 from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
 from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
-def login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
-            if user:
-                auth.login(request, user)
-                messages.success(request, 'Вы успешно вошли в систему.')
 
-                if request.POST.get('next', None):
-                    return HttpResponseRedirect(request.POST.get('next'))
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+    authentication_form = UserLoginForm
+    #success_url = reverse_lazy('main:index')
+
+    def get_success_url(self):
+        redirect_page = self.request.POST.get('next', None)
+        if redirect_page and redirect_page != reverse('users:logout'):
+            return redirect_page
+        return reverse_lazy('main:index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Авторизация'
+        return context
+
+
+# def login(request):
+#     if request.method == 'POST':
+#         form = UserLoginForm(data=request.POST)
+#         if form.is_valid():
+#             username = request.POST['username']
+#             password = request.POST['password']
+#             user = auth.authenticate(username=username, password=password)
+#             if user:
+#                 auth.login(request, user)
+#                 messages.success(request, 'Вы успешно вошли в систему.')
+
+#                 if request.POST.get('next', None):
+#                     return HttpResponseRedirect(request.POST.get('next'))
                 
-                return HttpResponseRedirect(reverse('main:index'))
-    else:
-        form = UserLoginForm()
+#                 return HttpResponseRedirect(reverse('main:index'))
+#     else:
+#         form = UserLoginForm()
 
-    context = {
-        'title': 'Авторизация',
-        'form': form
+#     context = {
+#         'title': 'Авторизация',
+#         'form': form
         
-    }
-    return render(request, 'users/login.html', context)
+#     }
+#     return render(request, 'users/login.html', context)
 
 
 def registration(request):
